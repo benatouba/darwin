5486c3c3f7dd6a32eb4dc97bdc41faa0c66e55d3
Benjamin Schmidt <benschmidt@live.de>
2022-09-19 21:16:01 +0200

    add notebooks to connect variables

--- constants.py
+++ constants.py
@@ -1,4 +1,7 @@
 from matplotlib.colors import LinearSegmentedColormap
+from pint import Unit
+import cf_xarray.units
+
 basepath = "/home/ben/data/GAR/"
 coordinates = {
     "rojas": (-0.618625, -90.3673),
@@ -14,6 +17,35 @@ coordinates = {
     "rosa": (-0.65453, -90.4035),
     # "met-e_bellavista": (-0.692384, -90.3282),
     # "met-e_puerto_ayora": (-0.743708, -90.3027)
+}
+
+measured_vars = {
+    "prcp": [
+        "PCP_tot_bucket",
+        "PCP_diff_radar",
+        "PCP_diffmin_radar",
+        "PCP_acoustic",
+    ],
+    "t2": "T",
+    "ws10": ["WS", "WSmax"],
+    "v10": ["WD"],
+    "u10": ["WD"],
+}
+
+measured_units = {
+    "PCP_tot_bucket": Unit("mm"),
+    "PCP_diff_radar": Unit("mm"),
+    "PCP_diffmin_radar": Unit("mm/min"),
+    "PCP_acoustic": Unit("mm"),
+    "T": Unit("C"),
+    "Pabs": Unit("hPa"),
+    "WS": Unit("m/s"),
+    "WSmax": Unit("m/s"),
+    "WD": Unit("degree"),
+    "FOG": Unit("g"),
+    "SLR": Unit("W/m2"),
+    "ST": Unit("C"),
+    "Vwc": Unit("count"),
 }
 
 color_map = LinearSegmentedColormap.from_list(
